8f1a4ea793f9d0c79208d9ce9f5d3e18c433d8d1
nongfugengxia <nongfugengxia@163.com>
2023-06-12 12:09:19 +0800

    增加岳麓区经纬度

--- traffic/main.m
+++ traffic/main.m
@@ -1,4 +1,16 @@
 
+addpath('automatic_scenario_generation');
+%% 
+% 导入岳麓区的路网
+minLat =  28.1625;
+maxLat =  28.2587;
+minLon = 112.7911;
+maxLon = 112.9604;
+bbox = [minLat maxLat;minLon maxLon];
+
+
+%% 
+
 % 导入场景
 sc = hutb_scenario;
 
