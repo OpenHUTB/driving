efa88dd94407d69bc889793ded94ac19a99ec692
zoudai <100852839+zoudai@users.noreply.github.com>
2023-07-27 17:06:34 +0800

    helperGenerateWaypoints.m

--- traffic/automatic_scenario_generation/helperGenerateWaypoints.m
+++ traffic/automatic_scenario_generation/helperGenerateWaypoints.m
@@ -1,17 +1,8 @@
 function waypoints=helperGenerateWaypoints(scenario,startPositions,goalPositions,varargin)
+%helperGenerateWaypoints 函数的主要功能是调用 helperWaypointsGen 来生成车辆的轨迹
 
-
-
-
-
-
-
-
-
-
-
-
-
+    %调用 helperWaypointsGen 来生成车辆的轨迹。它接受四个输入参数：scenario 表示驾驶场景，startPositions 是一个包含车辆起始位置的数组，goalPositions 是一个包含车辆目标位置的数组，varargin{:} 表示任意其他可选参数。
     traj=helperWaypointsGen(scenario,startPositions,goalPositions,varargin{:});
+    %从 traj 结构体中获取车辆的路径（路点）。
     waypoints=traj.Path;
 end
